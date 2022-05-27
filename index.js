3d8c96200c075f1f56ec069120ceb106df23505b
OzoneClub <cryptokuinak@gmail.com>
2022-05-27 09:05:37 +0100

    update-top-1000

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = "uniswap_v2_pairs";
 const chainName = "bsc";
+let db;
+let collection;
+
+const WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+
+const connectDB = async () => {
+    await client.connect();
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
+    collection = db.collection(chainName);
+};
 
 const moralisTokenApi = async (address) => {
     const response = await fetch(
@@ -59,13 +70,87 @@ const tokenInfoPCSV2Api = async (address, pairIndex) => {
     process.exit();
 };
 
+const getPairInfobyIndex = async (pairIndex, pcsV2Contract) => {
+    try {
+        const pairAddress = await pcsV2Contract.methods
+            .allPairs(pairIndex)
+            .call();
+        const pairContract = v2PairContract(pairAddress);
+        const [token0, token1, reserves] = await Promise.all([
+            pairContract.methods.token0().call(),
+            pairContract.methods.token1().call(),
+            pairContract.methods.getReserves().call(),
+        ]);
+
+        if (reserves._reserve0 == 0 || reserves._reserve1 == 0) return;
+        try {
+            const token0Contract = erc20Contract(token0);
+            const token1Contract = erc20Contract(token1);
+
+            const [token0Decimals, token1Decimals] = await Promise.all([
+                token0Contract.methods.decimals().call(),
+                token1Contract.methods.decimals().call(),
+            ]);
+
+            const [pcsV2ResultToken0, pcsV2ResultToken1] = await Promise.all([
+                tokenInfoPCSV2Api(token0, pairIndex),
+                tokenInfoPCSV2Api(token1, pairIndex),
+            ]);
+
+            let reserve_usd = 0;
+            if (token0 === WBNB_ADDRESS) {
+                reserve_usd =
+                    ((parseFloat(pcsV2ResultToken0.data.price) *
+                        reserves._reserve0) /
+                        10 ** token0Decimals) *
+                    2;
+            } // if (token1 === WBNB_ADDRESS)
+            else {
+                reserve_usd =
+                    ((parseFloat(pcsV2ResultToken1.data.price) *
+                        reserves._reserve1) /
+                        10 ** token1Decimals) *
+                    2;
+            }
+
+            const updateDBItem = {
+                pairIndex,
+                pairAddress,
+                token0,
+                token1,
+                token0Name: pcsV2ResultToken0.data.name,
+                token1Name: pcsV2ResultToken1.data.name,
+                token0Symbol: pcsV2ResultToken0.data.symbol,
+                token1Symbol: pcsV2ResultToken1.data.symbol,
+                token0Decimals,
+                token1Decimals,
+                reserve0: reserves._reserve0,
+                reserve1: reserves._reserve1,
+                token0Price: parseFloat(pcsV2ResultToken0.data.price),
+                token1Price: parseFloat(pcsV2ResultToken1.data.price),
+                reserve_usd,
+            };
+
+            // KConsole.cyan(updateDBItem);
+            // if (updateDBItem.reserve_usd ===)
+            await collection.updateOne(
+                { pairIndex: pairIndex },
+                { $set: updateDBItem },
+                { upsert: true }
+            );
+            return updateDBItem;
+        } catch (error) {
+            console.log("error with processing pair", pairAddress, pairIndex);
+            return;
+        }
+    } catch (error) {
+        console.log("error", pairIndex, error);
+        process.exit();
+    }
+};
 async function main() {
     let startPair = 0;
-    await client.connect();
-    console.log("Connected successfully to server");
-    const db = client.db(dbName);
-    const collection = db.collection(chainName);
-
+    await connectDB();
     const lastPairIndex = await collection
         .find()
         .sort({ pairIndex: -1 })
@@ -77,78 +162,6 @@ async function main() {
     else startPair = lastPairIndex[0].pairIndex;
     const pcsV2Contract = await factoryContract(V2_FACTORY_ADDRESS);
     const pairLength = await pcsV2Contract.methods.allPairsLength().call();
-
-    const getPairInfobyIndex = async (pairIndex) => {
-        try {
-            const pairAddress = await pcsV2Contract.methods
-                .allPairs(pairIndex)
-                .call();
-            const pairContract = v2PairContract(pairAddress);
-            const [token0, token1, reserves] = await Promise.all([
-                pairContract.methods.token0().call(),
-                pairContract.methods.token1().call(),
-                pairContract.methods.getReserves().call(),
-            ]);
-
-            if (reserves._reserve0 == 0 || reserves._reserve1 == 0) return;
-            try {
-                const token0Contract = erc20Contract(token0);
-                const token1Contract = erc20Contract(token1);
-
-                const [token0Decimals, token1Decimals] = await Promise.all([
-                    token0Contract.methods.decimals().call(),
-                    token1Contract.methods.decimals().call(),
-                ]);
-
-                const [pcsV2ResultToken0, pcsV2ResultToken1] =
-                    await Promise.all([
-                        tokenInfoPCSV2Api(token0, pairIndex),
-                        tokenInfoPCSV2Api(token1, pairIndex),
-                    ]);
-
-                const updateDBItem = {
-                    pairIndex,
-                    pairAddress,
-                    token0,
-                    token1,
-                    token0Name: pcsV2ResultToken0.data.name,
-                    token1Name: pcsV2ResultToken1.data.name,
-                    token0Symbol: pcsV2ResultToken0.data.symbol,
-                    token1Symbol: pcsV2ResultToken1.data.symbol,
-                    token0Decimals,
-                    token1Decimals,
-                    reserve0: reserves._reserve0,
-                    reserve1: reserves._reserve1,
-                    token0Price: parseFloat(pcsV2ResultToken0.data.price),
-                    token1Price: parseFloat(pcsV2ResultToken1.data.price),
-                    reserve_usd:
-                        ((parseFloat(pcsV2ResultToken0.data.price) *
-                            reserves._reserve0) /
-                            10 ** token0Decimals) *
-                        2,
-                };
-
-                // KConsole.cyan(updateDBItem);
-                // if (updateDBItem.reserve_usd ===)
-                await collection.updateOne(
-                    { pairIndex: pairIndex },
-                    { $set: updateDBItem },
-                    { upsert: true }
-                );
-                return updateDBItem;
-            } catch (error) {
-                console.log(
-                    "error with processing pair",
-                    pairAddress,
-                    pairIndex
-                );
-                return;
-            }
-        } catch (error) {
-            console.log("error", pairIndex, error);
-            process.exit();
-        }
-    };
 
     if (process.argv[2]) startPair = process.argv[2];
 
@@ -162,7 +175,9 @@ async function main() {
         KConsole.cyan(`processing ${i} ~ ${idArr.at(-1)}`);
         // const pairInfoArr =
         await Promise.all(
-            idArr.map((pairIndex) => getPairInfobyIndex(pairIndex))
+            idArr.map((pairIndex) =>
+                getPairInfobyIndex(pairIndex, pcsV2Contract)
+            )
         );
         // console.log(pairInfoArr);
         KConsole.cyan(`updating database ${i} ~ ${idArr.at(-1)}: finished`);
@@ -170,4 +185,33 @@ async function main() {
 
     KConsole.cyan(pairLength);
 }
-main().then(console.log).catch(console.error).finally();
+async function updateTop1000() {
+    let skip = 0;
+    if (process.argv[3]) skip = process.argv[2];
+    await connectDB();
+    let top1000PairIndex = await collection
+        .find()
+        .sort({ reserve_usd: -1 }, { pairIndex: 1 })
+        .skip(skip)
+        .limit(1000)
+        .toArray();
+
+    const pcsV2Contract = await factoryContract(V2_FACTORY_ADDRESS);
+    const pairLength = await pcsV2Contract.methods.allPairsLength().call();
+    for (i = 0; i < top1000PairIndex.length; i += 100) {
+        let idArr = Array.from(
+            { length: 100 },
+            (_, offset) => top1000PairIndex.at(i + offset).pairIndex
+        ).filter((item) => item < pairLength);
+
+        KConsole.cyan(`processing 100 from `, i);
+        await Promise.all(
+            idArr.map((pairIndex) =>
+                getPairInfobyIndex(pairIndex, pcsV2Contract)
+            )
+        );
+    }
+}
+if (process.argv[2] === "update-top")
+    updateTop1000().then(console.log).catch(console.error).finally();
+else main().then(console.log).catch(console.error).finally();
